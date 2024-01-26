c81985522fa5b6cdc8d319038b138370d24b294d
Ajay Gurjar <ajaygurjar048@gmail.com>
2024-01-26 12:06:18 +0530

    f

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
     </div>
   );
 }
 
-export default App;
+function Avatar() {
+  return (
+    <div>
+      <img className="pt" src="Ajayp.jpg" alt="Ajay" />
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <>
+      <h1> Ajay Gurjar</h1>
+      <p>
+        {" "}
+        Student Of MITS Gwalior With Computer Science Stream . I Am Currently
+        Pursuing My Bachelor's Degree In May 2024. My Area Of Interest Is
+        Software Development.{" "}
+      </p>
+    </>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill name="React 💪" color="blue" />
+      <Skill name="Html/Css 💪" color="red" />
+      <Skill name="JavaScript 💪" color="green" />
+      <Skill name="NodeJs 👶" color="yellow" />
+      <Skill name="MongoDb 👼🏻" color="orange" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      {props.name}
+    </div>
+  );
+}
